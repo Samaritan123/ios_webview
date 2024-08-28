6f3d97f2a3b4787fa39f7a703ad76bef0fbbb426
kailliangtang <kailiangtang@pony.ai>
2024-08-28 15:06:59 +0800

    fix

---
+++ lib/webview_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String url;
+
+  const WebViewPage({super.key, required this.url});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebViewWidget(
+      controller: _webViewController,
+    );
+  }
+}
